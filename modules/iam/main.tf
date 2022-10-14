daabb159bd3e243e4d43831546d5fd7a660aa233
Rick Harp <rickharpjr@gmail.com>
2022-10-14 09:05:52 -0700

    Add files via upload

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,12 +1,16 @@
-data "aws_iam_policy_document" "admin-assume-role-policy" {
+data "aws_iam_policy_document" "instance-assume-role-policy" {
   statement {
-    actions = ["*"]
-    resources = ["*"]
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
   }
 }
 
-resource "aws_iam_role" "snyk-admin-role" {
-  name                = "snyk_${var.environment}_role"
-  assume_role_policy  = data.aws_iam_policy_document.admin-assume-role-policy.json # (not shown)
-  managed_policy_arns = []
+resource "aws_iam_role" "instance-role" {
+  name               = "snyk_${var.environment}_instance_role"
+  path               = "/system/"
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
 }
